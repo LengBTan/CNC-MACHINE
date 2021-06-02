f1f2815b47c4c2a946b2da2c548a1c8463700b36
leng <lengbountan@gmail.com>
2021-06-01 20:54:08 -0400

    added test for stepper motor

--- stepper.py
+++ stepper.py
@@ -1,7 +1,12 @@
+import time
 import gpiozero
 from RpiMotorLib import RpiMotorLib
 
-#define GPIO pins
+GPIO_pins=(14,15,18,23)
 
-GPIO_pins=()
+mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "yMotor")
+time.sleep(0.5)
+
+mymotortest.motor_run(GPIO_pins , 0.1, 50, False, False, "half", .05)
+
 
