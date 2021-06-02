a9e6036f5b65ab333c67871d726732a32e5d1aa8
Leng <lengbountan@gmail.com>
2021-06-02 01:05:24 -0400

    Created test for stepper movements

--- stepper.py
+++ stepper.py
@@ -1,5 +1,7 @@
+import sys
 import time
 import gpiozero
+import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 GPIO_pins=(14,15,18,23)
@@ -7,6 +9,8 @@ GPIO_pins=(14,15,18,23)
 mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "yMotor")
 time.sleep(0.5)
 
-mymotortest.motor_run(GPIO_pins , 0.1, 50, False, False, "half", .05)
+mymotortest.motor_run(GPIO_pins , 0.01, 20, False, True, "half", .05)
+mymotortest.motor_run(GPIO_pins , 0.01, 30, True, True, "half", .05)
 
+GPIO.cleanup()
 
