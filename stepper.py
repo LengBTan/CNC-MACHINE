eee5c941a072e000b6133fdb97702827beaf1d12
Leng <lengbountan@gmail.com>
2021-06-02 22:55:19 -0400

    added test for X and Z axis stepper motors

--- stepper.py
+++ stepper.py
@@ -4,13 +4,24 @@ import gpiozero
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
-GPIO_pins=(14,15,18,23)
+GPIOY_pins=(14,15,18,23)
+GPIOX_pins=(24,25,8,7)
+GPIOZ_pins=(1,12,16,20)
 
 mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "yMotor")
+##mymotortest = RpiMotorLib.BYJMotor("MyMotorTwo", "xMotor")
+##mymotortest = RpiMotorLib.BYJMotor("MyMotorThree", "zMotor")
 time.sleep(0.5)
 
-mymotortest.motor_run(GPIO_pins , 0.01, 20, False, True, "half", .05)
-mymotortest.motor_run(GPIO_pins , 0.01, 30, True, True, "half", .05)
+mymotortest.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
+mymotortest.motor_run(GPIOY_pins , 0.005, 60, True, True, "half", .05)
+
+mymotortest.motor_run(GPIOX_pins , 0.005, 60, False, True, "half", .05)
+mymotortest.motor_run(GPIOX_pins , 0.005, 60, True, True, "half", .05)
+
+mymotortest.motor_run(GPIOZ_pins , 0.005, 60, False, True, "half", .05)
+mymotortest.motor_run(GPIOZ_pins , 0.005, 60, True, True, "half", .05)
 
 GPIO.cleanup()
 
+sys.exit() 
