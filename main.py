0ed8803d6cb122f6b5a510e2abd92a832ab9f886
leng <lengbountan@gmail.com>
2021-06-09 00:25:24 -0400

    updated homed function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import threading
-import gpiozero
+from gpiozero import Button
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
@@ -21,32 +21,51 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIOX_pins[0],GPIO.OUT)
 GPIO.setup(y_endstop_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
-
-
 #Declare instance of a class to pass GPIO pins to RpiMotorLib
-YMotor = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
-XMotor = RpiMotorLib.BYJMotor("MyMotorTwo", "Nema")
-ZMotor = RpiMotorLib.BYJMotor("MyMotorThree", "Nema")
+yMotor = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
+xMotor = RpiMotorLib.BYJMotor("MyMotorTwo", "Nema")
+zMotor = RpiMotorLib.BYJMotor("MyMotorThree", "Nema")
 time.sleep(0.5)
 
-#YMotor.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
-#YMotor.motor_run(GPIOY_pins , 0.005, 60, True, True, "half", .05)
+#yMotor.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
+#yMotor.motor_run(GPIOY_pins , 0.005, 60, True, True, "half", .05)
 
-#XMotor.motor_run(GPIOX_pins , 0.005, 60, False, False, "half", .05)
-#XMotor.motor_run(GPIOX_pins , 0.005, 60, True, False, "half", .05)
+#xMotor.motor_run(GPIOX_pins , 0.005, 60, False, False, "half", .05)
+#xMotor.motor_run(GPIOX_pins , 0.005, 60, True, False, "half", .05)
 
-#ZMotor.motor_run(GPIOZ_pins , 0.005, 60, False, False, "half", .05)
-#ZMotor.motor_run(GPIOZ_pins , 0.005, 60, True, False, "half", .05)
+#zMotor.motor_run(GPIOZ_pins , 0.005, 60, False, False, "half", .05)
+#zMotor.motor_run(GPIOZ_pins , 0.005, 60, True, False, "half", .05)
 
 def home():
-    ySwitch = False
-    xSwitch = False
-    zswitch = False
-    while ySwitch == False :
-        ##YMotor.motor_run(GPIOY_pins , 0.005, 10, False, True, "half", .05)
-        if(ySwitch==True):
-            print("homed")
-            break
+    ySwitch=Button(2)
+    xSwitch=Button(3)
+    zSwitch=Button(4)
+    ySwitchIsPressed = False
+    xSwitchIsPressed = False
+    zSwitchIsPressed = False
+    while ((ySwitchIsPressed == False) and (xSwitchIsPressed == False) and (zSwitchIsPressed == False)) :
+        if(ySwitch.is_pressed):
+            print("y homed")
+            ySwitchIsPressed = True
+        else:
+            print("ymotortest")
+            #yMotor.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
+
+        if(ySwitch.is_pressed):
+            print("x homed")
+            xSwitchIsPressed = True
+        else:
+            print("xmotortest")
+            #xMotor.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
+
+        if(ySwitch.is_pressed):
+            print("z homed")
+            zSwitchIsPressed = True
+        else:
+            print("zmotortest")
+            #zMotor.motor_run(GPIOY_pins , 0.005, 60, False, True, "half", .05)
+
+
 
 
 
