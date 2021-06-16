7bd41c516b0fbe90391a8d42d6b143626dd4aa17
leng <lengbountan@gmail.com>
2021-06-16 04:08:42 -0400

    made comments

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ x_endstop_pin = 2
 y_endstop_pin = 3
 z_endstop_pin = 4
 
-#Global variables
+#Global variables accessed by all functions
 global xCoord
 global yCoord
 global zCoord
@@ -28,23 +28,31 @@ xCoord = 0
 yCoord = 0
 zCoord = 0
 
-#Declare instance of a class to pass GPIO pins to RpiMotorLib
+#Declare instance of a class to pass GPIO pins to RpiMotorLib library
 yMotor = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
 xMotor = RpiMotorLib.BYJMotor("MyMotorTwo", "Nema")
 zMotor = RpiMotorLib.BYJMotor("MyMotorThree", "Nema")
 time.sleep(0.5)
 
+
+# Home function. This homes the X, Y and Z
+# motors and stores the origin point as (0,0,0)
+# Three limit switches are used to home each axes.
 def home():
-    ySwitch=Button(2)
-    xSwitch=Button(3)
-    zSwitch=Button(4)
-    ySwitchIsPressed = False
+    global xCoord
+    global yCoord
+    global zCoord    
+    ySwitch=Button(2) # gpio2 pin
+    xSwitch=Button(3) # gpio3 pin
+    zSwitch=Button(4) # gpio4 pin
+    ySwitchIsPressed = False #stores boolean, tells the axes limit switches that they are not pressed
     xSwitchIsPressed = False
     zSwitchIsPressed = False
-    while ((ySwitchIsPressed == False)) :
-        if(ySwitch.is_pressed):
+    while ((ySwitchIsPressed == False)) : # continuously checks if the limit switch is pressed. Moves the motor if the limit switch is not pressed
+        if(ySwitch.is_pressed): # checks if limit switch is pressed, and sets the SwitchIsPressed to true, and sets the coordinates to the origin, else it moves the motor.
             print("y homed")
             ySwitchIsPressed = True
+            xCoord = 0
         else:
             time.sleep(.1)
             yMotor.motor_run(GPIOY_pins , 0.01, 1, True, False, "half", .05)
@@ -53,6 +61,7 @@ def home():
         if(xSwitch.is_pressed):
             print("x homed")
             xSwitchIsPressed = True
+            yCoord = 0
         else:
             time.sleep(.1)
             xMotor.motor_run(GPIOX_pins, 0.01, 1, True, False , "half", .05)
@@ -61,18 +70,23 @@ def home():
         if(zSwitch.is_pressed):
             print("z homed")
             zSwitchIsPressed = True
+            zCoord = 0
         else:
             time.sleep(.1)
             zMotor.motor_run(GPIOZ_pins , 0.01, 1, True, False, "half", .05)
     print("sucessfully homed")
 
+
+# Move function. It moves the three diffrent axes of the CNC machine.
+# pos1 parameter is the current coordinate for the motor at the specified axis.
+# pos2 parameter is the next coordinate the motor will move to for the specified axis.
 def moveX(pos1, pos2):
     global xCoord
-    if(pos1 < pos2):
+    if(pos1 < pos2): #checks if pos1 is smaller than pos2 and finds the diffrence between pos2 and pos1.
         finalPos = pos2 - pos1
-        xCoord = pos1 + finalPos
+        xCoord = pos1 + finalPos # stores the current x coordinate 
         print((finalPos), " CW X MOTOR")
-    if(pos1 > pos2):
+    if(pos1 > pos2): # checks if pos1 is larger than pos2 and finds the diffrence between pos1 and pos2
         finalPos = pos1 - pos2
         xCoord = pos1 - finalPos
         print((finalPos), " CCW X MOTOR")
@@ -99,30 +113,36 @@ def moveZ(pos1, pos2):
         zCoord = pos1 - finalPos
         print((finalPos), " CCW Z MOTOR")
 
+
+# Draw function. It opens a file containing coordinates for the 3 motor axes to move to.
+# filename parameter stores the user entered filename, and reads it using open().
+# It checks whether the x,y or z motor needs to move.
 def draw(filename):
-    f = open(filename, "r")
-    data = f.read()
-    lines = data.splitlines()
-    for i,line in enumerate(lines):
-        if "X" in line:
+    f = open(filename, "r") #opens file
+    data = f.read() #reads file
+    lines = data.splitlines() # splits the lines into seperate strings
+    for i,line in enumerate(lines): #reads each line to the end of the file
+        if "X" in line: # checks if X is in the line
             xNext = line[1:]
-            #print(xNext)
+            #print(i,xNext)
             moveX(xCoord, xNext)
-        if "Y" in line:
+        if "Y" in line: # checks if Y is in the line
             yNext = line[1:]
-            #print(yNext)
+            #print(i,yNext)
             moveY(yCoord, yNext)
-        if "Z" in line:
+        if "Z" in line: # checks if Z is in the line
             zNext = line[1:]
-            #print(zNext)
+            #print(i,zNext)
             moveZ(zCoord, zNext)
     f.close
 
+
+# main function
 if __name__ == "__main__":
-    while True:
+    while True: #loop
         filename = input("input")
-        try:
-            home()
+        try: # Attempts to read filename using the draw function, if unable, prompts user that it could not load the file. 
+            home()#maybe move to draw function
             draw(filename)
         except IOError:
             print("Could not find or load file")
