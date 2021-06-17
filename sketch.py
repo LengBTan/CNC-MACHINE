e90df0af624e56c85e0322e6d3f045325ff2df22
leng <lengbountan@gmail.com>
2021-06-17 19:19:34 -0400

    Created sketch.py plotting software

---
+++ sketch.py
@@ -0,0 +1,152 @@
+#import libraries
+import tkinter as tk
+from tkinter import *
+import turtle
+
+class Sketch(tk.Tk):
+    def __init__(self):
+        global xCoord
+        global yCoord
+        xCoord = 0
+        yCoord = 0
+
+        super().__init__()
+        self.title('Sketch')
+        self.geometry('700x600')
+        
+        self.canvas = tk.Canvas()
+        self.canvas.config(width=500,height=500)
+        self.canvas.pack(side=tk.LEFT)
+        self.screen = turtle.TurtleScreen(self.canvas)
+
+        self.upButton = tk.Button(self, text="Up", command= self.moveUp, width=5, height=1, compound="c")
+        self.upButton.place(x=550,y=0)
+        self.leftButton = tk.Button(self, text="Left", command= self.moveLeft, width=5, height=1, compound="c")
+        self.leftButton.place(x=500,y=20)
+        self.rightButton = tk.Button(self, text="Right", command= self.moveRight, width=5, height=1, compound="c")
+        self.rightButton.place(x=600,y=20)
+        self.downButton = tk.Button(self, text="Down", command= self.moveDown, width=5, height=1, compound="c")
+        self.downButton.place(x=550,y=40)
+
+        self.penOff = tk.Button(self, text="Pen Up", command= self.penOff, width=8, height=2, compound="c")
+        self.penOff.place(x=540,y=70)
+        self.penOn = tk.Button(self, text="Pen Down", command= self.penOn, width=8, height=2, compound="c")
+        self.penOn.place(x=540,y=110)
+
+        self.penUndo = tk.Button(self, text="UNDO", command= self.penUndo, width=8, height=2, compound="c")
+        self.penUndo.place(x=540,y=150)
+
+        self.penClear = tk.Button(self, text="CLEAR", command= self.penClear, width=8, height=2, compound="c")
+        self.penClear.place(x=540,y=200)
+
+        self.pen = turtle.RawTurtle(self.screen)
+        self.pen.reset()
+        self.pen.penup()
+        self.pen.hideturtle()
+        self.pen.goto(-240,-240)
+        self.pen.pendown()
+        self.pen.showturtle()
+        xCoord = 0
+        yCoord = 0
+
+
+        self.instructions = Text(width=10,height=10)
+        self.instructions.place(x=510,y=250)
+        #self.instructions.insert(END, "\na")
+        #self.instructions.insert(END, "\nb")
+
+
+    def moveUp(self):
+        #print(self.pen.ycor())
+        global yCoord
+        global penDown
+        self.pen.setheading(90)
+        self.pen.forward(10)
+        if(self.pen.ycor()>240):
+            self.pen.undo()
+        else:
+            yCoord += 1
+            self.instructions.insert(INSERT,"Y")
+            self.instructions.insert(INSERT,yCoord)
+            self.instructions.insert(END,"\n")
+            print(yCoord)
+
+
+    def moveLeft(self):
+        global xCoord
+        self.pen.setheading(180)
+        self.pen.forward(10)
+        if(self.pen.xcor()< (-240)):
+            self.pen.undo()
+        else:
+            xCoord -=1
+            self.instructions.insert(INSERT,"X")
+            self.instructions.insert(INSERT,xCoord)
+            self.instructions.insert(END,"\n")
+            print(xCoord)
+
+    def moveRight(self):
+        global xCoord
+        self.pen.setheading(0)
+        self.pen.forward(10)
+        if(self.pen.xcor()>240):
+            self.pen.undo()
+        else:
+            xCoord +=1
+            self.instructions.insert(INSERT,"X")
+            self.instructions.insert(INSERT,xCoord)
+            self.instructions.insert(END,"\n")
+            print(xCoord)
+    
+    def moveDown(self):
+        global yCoord
+        global penDown
+        self.pen.setheading(270)
+        self.pen.forward(10)
+        print(self.pen.ycor())
+        if(self.pen.ycor()<-240):
+            self.pen.undo()
+        else:
+            yCoord -= 1
+            self.instructions.insert(INSERT,"Y")
+            self.instructions.insert(INSERT,yCoord)
+            self.instructions.insert(END,"\n")
+            print(yCoord)
+
+    def penOn(self):
+
+        self.pen.pendown()
+        self.instructions.insert(INSERT,"Z0")
+        self.instructions.insert(END,"\n")
+
+    def penOff(self):
+        self.pen.penup()
+        self.instructions.insert(INSERT,"Z10")
+        self.instructions.insert(END,"\n")
+    
+    def penUndo(self):
+        self.pen.undo()
+        
+    
+    def penClear(self):
+        global xCoord
+        global yCoord
+        self.pen.reset()
+        self.pen.penup()
+        self.pen.hideturtle()
+        self.pen.goto(-240,-240)
+        self.pen.pendown()
+        self.pen.showturtle()
+        xCoord = 0
+        yCoord = 0
+        self.instructions.delete('1.0',END)
+
+    
+    
+
+
+
+    
+if __name__ == "__main__":
+    window = Sketch()
+    window.mainloop()
