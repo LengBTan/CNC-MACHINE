c9e415e3a08f83b8fe2415e88693ff0110693be2
leng <lengbountan@gmail.com>
2021-06-17 23:04:40 -0400

    Made comments

--- sketch.py
+++ sketch.py
@@ -1,24 +1,33 @@
-#import libraries
+# import libraries
 import tkinter as tk
 from tkinter import *
 import turtle
 
+# Sketch class. Sets up the GUI and Turtle drawing interface.
+# tk.Tk parameter is the tkinter gui library
 class Sketch(tk.Tk):
+
+    # Initialize variables, window, canvas, and buttons for the current instance of the class.
+    # self parameter - current instance of the class.
     def __init__(self):
+
+        # Stores X and Y coordinates, accessed by diffrent functions.
         global xCoord
         global yCoord
         xCoord = 0
         yCoord = 0
 
-        super().__init__()
+        super().__init__() #returns object as parent class
         self.title('Sketch')
         self.geometry('700x600')
         
+        #setup canvas
         self.canvas = tk.Canvas()
         self.canvas.config(width=500,height=500)
         self.canvas.pack(side=tk.LEFT)
-        self.screen = turtle.TurtleScreen(self.canvas)
+        self.screen = turtle.TurtleScreen(self.canvas) # place Turtle in tkinter as a widget. 
 
+        #setup button size and placement on the window
         self.upButton = tk.Button(self, text="Up", command= self.moveUp, width=5, height=1, compound="c")
         self.upButton.place(x=550,y=0)
         self.leftButton = tk.Button(self, text="Left", command= self.moveLeft, width=5, height=1, compound="c")
@@ -32,16 +41,18 @@ class Sketch(tk.Tk):
         self.penOff.place(x=540,y=70)
         self.penOn = tk.Button(self, text="Pen Down", command= self.penOn, width=8, height=2, compound="c")
         self.penOn.place(x=540,y=110)
-
+        
         self.fileWrite = tk.Button(self, text="Export", command= self.fileWrite, width=8, height=2, compound="c")
         self.fileWrite.place(x=540,y=150)
 
         self.penClear = tk.Button(self, text="CLEAR", command= self.penClear, width=8, height=2, compound="c")
         self.penClear.place(x=540,y=200)
 
+        #setup text field size and position
         self.instructions = Text(width=12,height=20)
         self.instructions.place(x=510,y=280)
 
+        #move pen to bottom left corner instead of middle of screen (origin)
         self.pen = turtle.RawTurtle(self.screen)
         self.pen.reset()
         self.pen.penup()
@@ -52,13 +63,15 @@ class Sketch(tk.Tk):
         xCoord = 0
         yCoord = 0
 
+    # moveUp/Left/Right/Down direction button action.
+    # Is called after a button is pressed from __init__
 
     def moveUp(self):
         #print(self.pen.ycor())
         global yCoord
         self.pen.setheading(90)
         self.pen.forward(10)
-        if(self.pen.ycor()>240):
+        if(self.pen.ycor()>240): # Checks if the coordinate of the Y position is more than 240. If it is, it undos the last pen movement, else it move the pen, adds 1 to the current Y coordinate, and inserts the current coordinate into the text field.
             self.pen.undo()
         else:
             yCoord += 1
@@ -66,7 +79,6 @@ class Sketch(tk.Tk):
             self.instructions.insert(INSERT,yCoord)
             self.instructions.insert(END,"\n")
             #print(yCoord)
-
 
     def moveLeft(self):
         global xCoord
@@ -108,6 +120,7 @@ class Sketch(tk.Tk):
             self.instructions.insert(END,"\n")
             #print(yCoord)
 
+    # pen On/Off button action. When pressed, pen is raised, or lowered, and inserts Z coordinates of the pen.
     def penOn(self):
 
         self.pen.pendown()
@@ -119,13 +132,14 @@ class Sketch(tk.Tk):
         self.instructions.insert(INSERT,"Z10")
         self.instructions.insert(END,"\n")
     
+    #fileWrite button action. When "export" button is pressed, creates a new file, and exports the coordinate text field into the new file.
     def fileWrite(self):
         file = self.instructions.get('1.0', END)
         f = open("file.txt", "w+")
         f.write(file)
         f.close()
 
-    
+    #When clear button is pressed, clears the canvas, and moves the pen cursor to the bottom left of the screen.
     def penClear(self):
         global xCoord
         global yCoord
@@ -139,7 +153,7 @@ class Sketch(tk.Tk):
         yCoord = 0
         self.instructions.delete('1.0',END)
 
-    
+    #main, opens the GUI
 if __name__ == "__main__":
     window = Sketch()
     window.mainloop()
